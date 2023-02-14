b1924bdc90817370f1c64874a6397e68729ce3e4
P2666410 <P2666410@CLPX0013WIN016W>
2023-02-14 17:14:07 +0000

    Add testing for adress class

---
+++ Testing4/UnitTest3.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsCustomer customer = new clsCustomer();
+            Assert.IsNotNull(customer);
+        }
+    }
+}
