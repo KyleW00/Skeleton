63b7447f4d26e9fa9c34af20ea101a7d95537571
LewisB02 <P2669629@my365.dmu.ac.uk>
2023-02-23 16:47:24 +0000

    Added Write to StaffViewer for Staff_Name

--- AdminSystem/StaffMViewer.aspx.cs
+++ AdminSystem/StaffMViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of the Staff class
+        clsStaff AStaff = new clsStaff();
+        //gets data from the session object
+        AStaff = (clsStaff)Session["AStaff"];
+        //displays a inputted data
+        Response.Write(AStaff.Staff_Name);
     }
 }
