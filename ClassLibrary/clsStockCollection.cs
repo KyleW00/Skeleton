72314eeb865ef723cec0415181a6c600bed6729a
Kevin Kumbuckal <P2603563@my365.dmu.ac.uk>
2023-04-27 21:27:21 +0100

    dont merge. all the errors need fixing before submission.

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStockCollection
+    {
+        public clsStockCollection()
+        {
+        }
+
+        public List<clsStock> StockList { get; internal set; }
+    }
+}
